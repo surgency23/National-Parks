d381982c6b9aaf69a5f080ea2c0a0d7c01f8983c
Chris Halasa <adnanhalasa2012@gmail.com>
2018-02-23 14:03:15 -0500

    update

--- m2-w6d4-capstone/Capstone/CampingCLI.cs
+++ m2-w6d4-capstone/Capstone/CampingCLI.cs
@@ -10,11 +10,11 @@ namespace Capstone
 {
     public class CampingCLI
     {
-        
-        private string connectionString;
-        
 
-        
+        private string connectionString;
+
+
+
         public CampingCLI(string connectionString)
         {
             this.connectionString = connectionString;
@@ -24,19 +24,19 @@ namespace Capstone
         {
             ParkMenu();
 
-            while(true)
+            while (true)
             {
                 string userInput = Console.ReadLine();
 
                 switch (userInput.ToLower())
                 {
-                    
+
                 }
             }
         }
         public void ParkMenu()
         {
-            
+
 
             ParkDAL allParks = new ParkDAL(connectionString);
             List<ParkModel> compiledParks = allParks.GetAllParks();
@@ -45,9 +45,10 @@ namespace Capstone
                 Console.WriteLine($"{compiledParks[i].ParkId}) {compiledParks[i].ParkName}");
             }
             Console.WriteLine($"Press Q to Quit");
+            Console.WriteLine();
             string parkId = Console.ReadLine();
 
-         
+            Console.Clear();
             ParkInformation(parkId);
         }
 
@@ -64,10 +65,12 @@ namespace Capstone
                     Console.WriteLine(compiledList[i].ToString());
                 }
             }
+            
             ParkCampground(parkId);
         }
         public void ParkCampground(string parkId)
         {
+            Console.WriteLine();
             Console.WriteLine("Select a command");
             Console.WriteLine();
             Console.WriteLine("1) View CampGrounds");
@@ -77,15 +80,54 @@ namespace Capstone
 
             string userInput = Console.ReadLine();
 
-            //switch (userInput.ToLower())
-            //{
-            //    case "1":
-            //        CampGroundsInPark();
-            //        break;
-            //    case "2":
-            //        MakeReservation();
-            //        break;
-            //}
+            switch (userInput.ToLower())
+            {
+                case "1":
+                    ViewCampgrounds(parkId);
+                    ViewCampGroundMenu();
+                    break;
+                case "2":
+                    ViewCampgrounds(parkId);
+                    SearchReservationMenu();
+                    break;
+                    //    case "2":
+                    //        MakeReservation();
+                    //        break;
+                    //}
+            }
+
+        }
+        public void ViewCampgrounds(string parkId)
+        {
+            CampGroundDAL campground = new CampGroundDAL(connectionString);
+            List<CampgroundModel> campList = campground.CampgroundsInPark(parkId);
+            for (int i = 0; i < campList.Count; i++)
+            {
+                if (parkId == campList[i].ParkId.ToString())
+                {
+                    Console.WriteLine(campList[i].ToString());
+                }
+            }
+         
+        }
+        public void ViewCampGroundMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Select a Command");
+            Console.WriteLine(" 1) Search For Available Reservation");
+            Console.WriteLine(" 2) Return To Previous Screen");
+            Console.WriteLine();
+        }
+
+        public void SearchReservationMenu()
+        {
+            Console.Write("Which campground (enter 0 to cancel)? ");
+            string campgroundInput = Console.ReadLine();
+            Console.Write("What Is the Arrival Date?(mm/dd/yyyy) "); 
+            string arrivalInput = Console.ReadLine();
+            Console.Write("What is the Departure Date?(mm/dd/yyyy) ");
+            string departureInput = Console.ReadLine();
         }
     }
 }
+
---
+++ m2-w6d4-capstone/Capstone/DAL/ReservationDAL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Capstone.Models;
+using System.Data.SqlClient;
+
+namespace Capstone.DAL
+{
+    public class ReservationDAL
+    {
+        private string connectionString;
+        private const string SQL_GetSites = "SELECT site.site_id, site.max_occupancy, site.accessible, site.utilities, campground.daily_fee FROM SITE JOIN campground ON campground.campground_id = site.campground_id WHERE campground.campground_id = @campground_id";
+
+        public ReservationDAL(string dbconnectionString)
+        {
+            connectionString = dbconnectionString;
+        }
+        public List<SiteModel> ReservationSearch(string campgroundId, string startDate, string departDate)1
+        {
+            List<SiteModel> availableSites = new List<SiteModel>();
+
+
+            return availableSites;
+        }
+
+     
+    }
+}
